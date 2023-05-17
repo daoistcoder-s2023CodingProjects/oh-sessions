15dd3ae2b9ca1cad486b2392b36c444726b8865e
daoistcoder <deservekindness@gmail.com>
2023-05-17 17:54:07 +0800

    update .env
    chang APP_NAME to Dog Lover's PhotoBlog

--- oh-laravel-practice/resources/views/welcome.blade.php
+++ oh-laravel-practice/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DL PhotoBlog</title>
+        <title>Dog Lover's PhotoBlog</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
