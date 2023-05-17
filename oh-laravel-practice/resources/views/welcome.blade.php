b28d7cae0bcb98a7c7be70cffef84b65b94d0f1c
daoistcoder <deservekindness@gmail.com>
2023-05-17 17:35:45 +0800

    optimize website
    change jpg to webp

--- oh-laravel-practice/resources/views/welcome.blade.php
+++ oh-laravel-practice/resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
                         <a href="/" class="scale-100 p-6 bg-card-test dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="h-32 w-32 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                    <img src="/svg/brunoPFP.jpg" alt="bruno-profile-pic" class="h-32 w-32 rounded-full">
+                                    <img src="/svg/brunoPFP.webp" alt="bruno-profile-pic" class="h-32 w-32 rounded-full">
                                 </div>
 
                                 <h2 class="mt-6 text-xl font-semibold text-gray-test dark:text-white">Bruno</h2>
@@ -56,7 +56,7 @@
                         <a href="/" class="scale-100 p-6 bg-card-test dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="h-32 w-32 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                    <img src="/svg/brownyPFP.jpg" alt="bruno-profile-pic" class="h-32 w-32 rounded-full">
+                                    <img src="/svg/brownyPFP.webp" alt="bruno-profile-pic" class="h-32 w-32 rounded-full">
 
                                 </div>
 
@@ -71,7 +71,7 @@
                         <a href="/" class="scale-100 p-6 bg-card-test dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="h-32 w-32 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                    <img src="/svg/niegroPFP.jpg" alt="bruno-profile-pic" class="h-32 w-32 rounded-full">
+                                    <img src="/svg/niegroPFP.webp" alt="bruno-profile-pic" class="h-32 w-32 rounded-full">
                                 </div>
 
                                 <h2 class="mt-6 text-xl font-semibold text-gray-test dark:text-white">Nieggro</h2>
