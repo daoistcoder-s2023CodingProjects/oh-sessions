948036bcc5fe90fea6807584387652cf396b2605
daoistcoder <deservekindness@gmail.com>
2023-05-21 23:17:26 +0800

    add test.blade w controlller
    congfigured in web.php

--- oh-laravel-practice/resources/views/test.blade.php
+++ oh-laravel-practice/resources/views/test.blade.php
@@ -6,17 +6,7 @@
         <div class="col-md-12">
             <h1>Album</h1>
             <div class="row">
-                @foreach($photos as $photo)
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                        <img src="{{ $photo->image }}" class="card-img-top" alt="Photo">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $photo->title }}</h5>
-                            <p class="card-text">{{ $photo->description }}</p>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
+
             </div>
         </div>
     </div>
--- oh-laravel-practice/routes/web.php
+++ oh-laravel-practice/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-
+use App\Http\Controllers\TestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
